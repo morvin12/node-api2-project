9839cd01140c956932cb7323b1ea1570e09b6802
Michael Orvin <morvin1212@gmail.com>
2022-01-11 17:59:43 -0700

    Router request can get all users and users by specific id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,13 +1,36 @@
 // implement your posts router here
 const express = require('express')
-
+const Post = require('./posts-model')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    
+    Post.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            error: err.message,
+        })
+    })
 })
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            error: err.message,
+        })
+    }
 })
 router.post('/', (req, res) => {
 
@@ -21,6 +44,5 @@ router.put('/:id', (req, res) => {
 router.get('/:id/messages', (req, res) => {
 
 })
-
 
 module.exports = router
